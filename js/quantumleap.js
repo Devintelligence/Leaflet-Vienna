ef3e0d8b863e4ef8d5527de8bcb66674afc720fa
ravishankar-19901113 <ravishankar.19901113@gmail.com>
2019-11-14 15:15:25 +0530

    Indentation fix

--- js/quantumleap.js
+++ js/quantumleap.js
@@ -1,36 +1,36 @@
 $(document).ready(function(){
-  $(".submitButton").click(function(){
-  	var entityID = $(this).closest('.entityClass').find('.entityID').val();
-  	var fromDate = $(this).closest('.entityClass').find('.startDate').val();
-  	var toDate = $(this).closest('.entityClass').find('.endDate').val();
-  	if(!fromDate){
-  		alert("Please select start date.")
-  		return false;
-  	}
-  	var data = {'fromDate': fromDate, 'attrs': 'temperature'}
-  	if(toDate){
-	  	if(new Date(fromDate) > new Date(toDate)){
-	  		alert("Start date should be less than end date.")
-  			return false;
-  		}
-  		if(new Date(fromDate) != new Date(toDate)){
-  			data['toDate'] = toDate
-  		}
-  	}
-  	var url = 'http://localhost:8668/v2/entities/' + entityID
-  	$.ajax({
-	    url: url,
-	    headers: {'Accept': 'application/json'},
-	    type: "GET", 
-	    data: data,
-	    success: function (result) {
-	    	downloadJson(result, entityID);
-	    },
-	    error: function (result) {
-	      downloadJson(result.responseJSON, entityID);
-	    }   
+	$(".submitButton").click(function(){
+		var entityID = $(this).closest('.entityClass').find('.entityID').val();
+		var fromDate = $(this).closest('.entityClass').find('.startDate').val();
+		var toDate = $(this).closest('.entityClass').find('.endDate').val();
+		if(!fromDate){
+			alert("Please select start date.")
+			return false;
+		}
+		var data = {'fromDate': fromDate, 'attrs': 'temperature'}
+		if(toDate){
+			if(new Date(fromDate) > new Date(toDate)){
+				alert("Start date should be less than end date.")
+				return false;
+			}
+			if(new Date(fromDate) != new Date(toDate)){
+				data['toDate'] = toDate
+			}
+		}
+		var url = 'http://localhost:8668/v2/entities/' + entityID
+		$.ajax({
+			url: url,
+			headers: {'Accept': 'application/json'},
+			type: "GET", 
+			data: data,
+			success: function (result) {
+				downloadJson(result, entityID);
+			},
+			error: function (result) {
+		  		downloadJson(result.responseJSON, entityID);
+			}   
 		}); 
-  });
+	});
 });
 
 function downloadJson(resultdata, entityID) {
