b5038e4e691058276d1e2793fb77c1adbaf8134f
root <ilari.mikkonen@apinf.io>
2019-12-17 21:09:37 +0200

    new csvuploader service

--- e-post/src/app.py
+++ e-post/src/app.py
@@ -7,7 +7,7 @@ import threading
 import logging
 
 from flask import Flask, render_template, request, redirect, url_for
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/files/'
 ALLOWED_EXTENSIONS = {'csv'}
@@ -15,40 +15,13 @@ ALLOWED_EXTENSIONS = {'csv'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/upload')
-def upload():
-    return render_template('upload.html')
-
-@app.route('/')
-def index():
-    return redirect(url_for('upload'))
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        form_name = request.form.get('FormName')
-        file = request.files.get('epost_file') or request.files.get('haufgasse_file')
-
-        filename = secure_filename(file.filename)
-        if file and allowed_file(file.filename):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            fun = 'process_%s' % form_name
-            x = threading.Thread(target=eval(fun), args=(filename,))
-            x.start()
-            return "File upload success"
-
-    return "File upload success"
-
+# -- form handlers --
 def process_haufgasse(filename):
     orionUri = os.getenv('orionUri', 'http://0.0.0.0:1026/') + 'v2/entities'
     headers = {
         "Content-Type": "application/json", 'Accept': 'application/json',
         "fiware-service": "vienna_buildings"
-    } 
+    }
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     with open(file_path) as f:
         file_data = csv.reader(f)
@@ -152,10 +125,43 @@ def process_epost(filename):
                 pass
     print('--------------- epost file completed ---------- ')
 
+# Have a map handy to get function from form name.
+form_handlers = {
+    'haufgasse' : process_haufgasse,
+    'epost' : process_epost
+}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/')
+def index():
+    return redirect(url_for('upload'))
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        form_name = request.form.get('FormName')
+        file = request.files.get('epost_file') or request.files.get('haufgasse_file')
+
+        filename = secure_filename(file.filename)
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            fun = form_handlers[form_name]
+            x = threading.Thread(target=fun, args=(filename,))
+            x.start()
+            return "File upload success"
+
+    return "File upload success"
 
 if __name__ == "__main__":
 
     if not os.path.exists(UPLOAD_FOLDER):
         os.mkdir(UPLOAD_FOLDER)
 
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
+
